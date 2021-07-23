2a133c6dda23a75c4cb0d312998222d087f1ada9
idttm <andrew79911997@gmail.com>
2021-07-23 18:39:33 +0300

    Initial Commit

---
+++ transitionUInavigationController/CollectionViewController.swift
@@ -0,0 +1,83 @@
+//
+//  CollectionViewController.swift
+//  transitionUInavigationController
+//
+//  Created by Andrew Cheberyako on 23.07.2021.
+//
+
+import UIKit
+import Hero
+
+
+
+class CollectionViewController: UICollectionViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        
+    }
+
+
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        cell.heroID = nil
+    
+        return cell
+    }
+    
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.visibleCells.forEach { cell in
+            cell.heroID = nil
+        }
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+
+        cell.heroID = "view"
+        let viewcontroller2 = ViewController()
+        viewcontroller2.heroModalAnimationType = .zoom
+        viewcontroller2.modalPresentationStyle = .fullScreen
+        self.navigationController?.present(viewcontroller2, animated: true, completion: nil)
+    }
+
+}
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow: CGFloat = 2
+        let paddingWidth = 20 * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem * 2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+}
+
---
+++ transitionUInavigationController/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  transitionUInavigationController
+//
+//  Created by Andrew Cheberyako on 23.07.2021.
+//
+
+import UIKit
+import Hero
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hero.isEnabled = true
+        self.redView.hero.isEnabled = true
+        view.heroID = "view"
+        view.backgroundColor = .green
+        redView.backgroundColor = .gray
+//        view.hero.modifiers = [.translate(y:100)]
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
